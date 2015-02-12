ca6e076cb796b949b4957b4d2ca8a4cbe69f18fd
lelouchmatlab <qhlegendzh@gmail.com>
2015-02-11 20:22:31 -0500

    IBM 2 with lower case

--- hw1/train_IBM1.py
+++ hw1/train_IBM1.py
@@ -2,6 +2,7 @@ import optparse
 import sys
 from collections import defaultdict
 import random
+#import pickle
 
 optparser = optparse.OptionParser()
 optparser.add_option("-b", "--bitext", dest="bitext", default="data/dev-test-train.de-en", help="Parallel corpus (default data/dev-test-train.de-en)")
@@ -10,32 +11,20 @@ optparser.add_option("-i", "--num_iteration", dest="num_iter", default=6, type="
 (opts, _) = optparser.parse_args()
 
 
-train_text = [[sentence.strip().split() for sentence in pair.split(' ||| ')] for pair in open(opts.bitext)][:opts.num_sents]
-test_text=[[sentence.strip().split() for sentence in pair.split(' ||| ')] for pair in open(opts.bitext)][:150]
+train_text = [[sentence.lower().strip().split() for sentence in pair.split(' ||| ')] for pair in open(opts.bitext)][:opts.num_sents]
+test_text=[[sentence.lower().strip().split() for sentence in pair.split(' ||| ')] for pair in open(opts.bitext)][:150]
 
 
 #f=lambda :random.random()/10
 f=lambda :1.0/10
 Prob_eg=defaultdict(lambda:defaultdict(f))   #P[g_w][e_w]
-#c_g=defaultdict(float)
-
-
-#vocab_g=defaultdict(set)
-#for (n,(g,e)) in enumerate(train_text):
-#  for g_word in g:
-#    for e_word in e:
-#      vocab_g[g_word].add(e_word)
-      
-#for g_word in vocab_g.keys():
-#  length=len(vocab_g[g_word])
-#  for e_word in vocab_g[g_word]:
-#    t_e_g[e_word][g_word]=1.0/length
-
-
+sys.stderr.write("IBM 1....\n")
 for i in xrange(opts.num_iter):
-  
+  sys.stderr.write("Iter\n")
   Statis_eg=defaultdict(lambda:defaultdict(float))
   for(n,(g,e)) in enumerate(train_text):
+    if n % 500 == 0:
+      sys.stderr.write(".")
     for target_word_e in e:
       denominator=0
       for source_word_g in g:
@@ -50,9 +39,12 @@ for i in xrange(opts.num_iter):
       normalization=normalization+Statis_eg[source_word_g][target_word_e]
     for target_word_e in Prob_eg[source_word_g].keys():
       Prob_eg[source_word_g][target_word_e]=Statis_eg[source_word_g][target_word_e]/normalization
+  sys.stderr.write("\n") 
 
-    
 
+  #with open('IBM1.pickle','wb') as handle:
+    #pickle.dump(Prob_eg,handle)  
+"""
 for(g,e) in train_text:
   for (e_idx, target_word_e) in enumerate(e):
     best_idx=0
@@ -66,42 +58,52 @@ for(g,e) in train_text:
 """
 
 
-q=defaultdict(f)
-
-num_iter=20
+Prob_ali=defaultdict(lambda:defaultdict(f))
+sys.stderr.write("IBM 2....\n")
+num_iter=16
 for i in xrange(num_iter):
-  c_s_t=defaultdict(float)
-  c_t=defaultdict(float)
-  c_e_g=defaultdict(lambda:defaultdict(float))
-  c_g=defaultdict(float)
+  sys.stderr.write("Iter\n")
+  Statis_ali=defaultdict(lambda:defaultdict(float))
+  Statis_eg=defaultdict(lambda:defaultdict(float))
   for(n,(g,e)) in enumerate(train_text):
+    if n % 500 == 0:
+      sys.stderr.write(".")
     for (e_idx,target_word_e) in enumerate(e):
       denominator=0
       for (g_idx,source_word_g) in enumerate(g):
-	denominator=denominator+t_e_g[target_word_e][source_word_g]*q[(g_idx,e_idx,len(g),len(e))]
+	denominator=denominator+Prob_eg[source_word_g][target_word_e]*Prob_ali[(e_idx,len(g),len(e))][g_idx]
       for (g_idx,source_word_g) in enumerate(g):
-	delta=q[(g_idx,e_idx,len(g),len(e))]*t_e_g[target_word_e][source_word_g]/denominator
-	c_e_g[target_word_e][source_word_g]=c_e_g[target_word_e][source_word_g]+delta
-	c_g[source_word_g]=c_g[source_word_g]+delta
-	c_s_t[(g_idx,e_idx,len(g),len(e))]=c_s_t[(g_idx,e_idx,len(g),len(e))]+delta
-	c_t[(e_idx,len(g),len(e))]=c_t[(e_idx,len(g),len(e))]+delta
-  for target_word_e in t_e_g.keys():
-    for source_word_g in t_e_g[target_word_e].keys():
-      t_e_g[target_word_e][source_word_g]=c_e_g[target_word_e][source_word_g]/c_g[source_word_g]
-  for (j,i,l,m) in c_s_t.keys():
-    q[(j,i,l,m)]=c_s_t[(j,i,l,m)]/c_t[(i,l,m)]
+	delta=Prob_ali[(e_idx,len(g),len(e))][g_idx]*Prob_eg[source_word_g][target_word_e]/denominator
+	Statis_eg[source_word_g][target_word_e]=Statis_eg[source_word_g][target_word_e]+delta
+	Statis_ali[(e_idx,len(g),len(e))][g_idx]=Statis_ali[(e_idx,len(g),len(e))][g_idx]+delta
   
-for(g,e) in test_text:
+  for source_word_g in Prob_eg.keys():
+    normalization=0
+    for target_word_e in Prob_eg[source_word_g].keys():
+      normalization=normalization+Statis_eg[source_word_g][target_word_e]
+    for target_word_e in Prob_eg[source_word_g].keys():
+      Prob_eg[source_word_g][target_word_e]=Statis_eg[source_word_g][target_word_e]/normalization
+  for (e_idx,len_g,len_e) in Prob_ali.keys():
+    normalization=0
+    for g_idx in Prob_ali[(e_idx,len_g,len_e)].keys():
+      normalization=normalization+Statis_ali[(e_idx,len_g,len_e)][g_idx]
+    for g_idx in Prob_ali[(e_idx,len_g,len_e)].keys():
+      Prob_ali[(e_idx,len_g,len_e)][g_idx]=Statis_ali[(e_idx,len_g,len_e)][g_idx]/normalization
+  sys.stderr.write("\n") 
+  
+  #with open('IBM2.pickle','wb') as handle:
+    #pickle.dump((Prob_eg,Prob_ali),handle)
+for(g,e) in train_text:
   for (e_idx, target_word_e) in enumerate(e):
     best_idx=0
     score=0
     for (g_idx, source_word_g) in enumerate(g):
-      if q[(g_idx,e_idx,len(g),len(e))]*t_e_g[target_word_e][source_word_g]>score:
+      if Prob_ali[(e_idx,len(g),len(e))][g_idx]*Prob_eg[source_word_g][target_word_e]>score:
 	best_idx=g_idx
-	score=q[(g_idx,e_idx,len(g),len(e))]*t_e_g[target_word_e][source_word_g]
+	score=Prob_ali[(e_idx,len(g),len(e))][g_idx]*Prob_eg[source_word_g][target_word_e]
     sys.stdout.write("%i-%i " % (best_idx,e_idx))
   sys.stdout.write("\n") 
-"""
+
 
       
 
